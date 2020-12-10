4c2a47acf000dd949ac949e8236645c46e178722
Diego <sturbadiego25@outlook.it>
2020-12-10 21:19:17 +0100

    Struttura dati footer con istanza Vue js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,25 @@ var app = new Vue ({
     logo: 'logo-2.png',
     menu: ['Home', 'About us', 'Feature', 'Testimonials', 'Contact US'],
     search: 'fas fa-search',
-    cart: 'fas fa-shopping-cart'
+    cart: 'fas fa-shopping-cart',
+    button: 'Get Started'
   }
-})
+});
+
+// Istanza Vue per generare gli elementi nel FOOTER
+var root = new Vue ({
+  el: '#root',
+  data: {
+    logo: "img/35.png",
+    paragraph: "It is a long established fact that a reader will be destracted by the readable.",
+    copy: "Copyright",
+    year: '2018',
+    link: "Sofbox",
+    menu: "Menu",
+    aboutUS: "About Us",
+    quick: 'Quick Links',
+    itemMenu: ['About Us', 'Theme', 'Features', 'Pricing', 'Blog'],
+    itemAbout: ['About Sofbox', 'Road map', 'How it Work', 'Team', 'News'],
+    itemQuick: ['About Us', 'Theme', 'Features', 'Pricing', 'Blog']
+  }
+});
